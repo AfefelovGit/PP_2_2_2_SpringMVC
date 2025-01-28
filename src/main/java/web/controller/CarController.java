7db85914a88b3b0800125fb3cb1f5a93dc5228d4
Aleksei Fefelov <avvfefelov@yahoo.com>
2025-01-28 08:49:51 +0400

    PP_2_2_2_SpringMVC_v4

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,22 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.dao.CarDao;
 import web.models.Car;
+import web.service.CarService;
 
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    private final CarDao carDao;
+    final
+    CarService carService;
 
     @Autowired
-    public CarController(CarDao carDao) {
-        this.carDao = carDao;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(value = "/car")
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        List<Car> listCar = carDao.getCars(count);
+        List<Car> listCar = carService.getCars(count);
         model.addAttribute("cars", listCar);
         return "car/car";
     }
